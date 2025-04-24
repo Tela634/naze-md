4156864876adfe35fb2da5401c249e15faea32e3
Tela634 <veronicawaithira680@gmail.com>
2025-04-24 10:06:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /**
    * Create By Dika Ardnt.
    * Recode By Naze Dev
-   * Contact Me on wa.me/6282113821188
-   * Follow https://github.com/nazedev
+   * Contact Me on wa.me/254701309409
+   * Follow https://github.com/Tela634
 */
 
 const fs = require('fs')
@@ -19,39 +19,39 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.owner = ['254701309409']
+global.ownernomer = "254701309409"
+global.premium = ['254701309409']
+global.packname = 'Sticker By L E S T A'
+global.author = 'L E S T A - P R O'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
-global.jumlha = '999'
-global.jumhal = '100000000000000'
-global.jumlah = '1000000000'
-global.prefa = ['','!','.','#','&']
+global.jumlha = '9999999999999999'
+global.jumhal = '9999999999999999'
+global.jumlah = '100000000000000'
+global.prefa = ['!','!','.','#','&']
 global.sp = ''
 
 // Setting Mess
 global.mess = {
-    success: '✅Done',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    success: '✅Done Join my group for much more ➜ https://chat.whatsapp.com/KJmsWPbJury9sE7lb67AOi',
+    admin: 'Command for Group Admin !',
+    botAdmin: 'Bot Must be made admin !',
     premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    owner: ' Owner OF Bot COMMANDS ONLY👤',
+    group: 'Command Only in Groups👥!',
+    private: ' Text for Private Chat Only 🧖!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
-    error: 'Error!',
-    errapi: 'Error Mungkin Apikey Tidak Valid!',
-    errmor: 'Error Kesalahan Sistem',
+    wait: 'Loading🇰🇪🇰🇪🇰🇪🇰🇪,
+    error: 'Error in command!',
+    errapi: 'Invalid Valid📵!',
+    errmor: 'not in Sistem',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 
 // Limit
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 99999999999o
 }
 
 // Fake
@@ -59,15 +59,15 @@ global.thumb = fs.readFileSync('./media/image/naze.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
 
 // Url
-global.mygit = 'https://github.com/nazedev'
+global.mygit = 'https://github.com/Tela634'
 global.myyt = 'https://youtube.com/c/Nazedev'
 global.myytv = 'https://youtu.be/FAsL-Jy4qLc'
-global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
+global.mygc = "https://chat.whatsapp.com/KJmsWPbJury9sE7lb67AOi"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'NAZE BOT'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'LESTA-PRO'
+global.akulaku = 'E L I U D'
+global.ytname = 'YT LESTA' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
